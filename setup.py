fafbdd6f4ee31de7a849f77b679fda3b198937a7
elerac <maenosuke079@gmail.com>
2020-04-29 19:48:47 +0900

    add setup.py & change some of directories

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='EasyPySpin',
+    version='1.0.0',
+    description='cv2.VideoCapture like wrapper for FLIR Spinnaker SDK',
+    url='https://github.com/elerac/EasyPySpin',
+    author='Ryota Maeda',
+    author_email='maeda.ryota.elerac@gmail.com',
+    license='MIT',
+    packages=find_packages()
+)
