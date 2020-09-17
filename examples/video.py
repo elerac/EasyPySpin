18095724fe2239cb77023487c151a90dfacaf773
elerac <maenosuke079@gmail.com>
2020-09-17 18:44:48 +0900

    Remove argparse for simplicity

--- examples/video.py
+++ examples/video.py
@@ -1,20 +1,12 @@
+"""
+A simple example of capturing and displaying an image
+"""
 import EasyPySpin
 import cv2
-import argparse
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--index", type=int, default=0, help="Camera index (Default: 0)")
-    parser.add_argument("-e", "--exposure",type=float, default=-1, help="Exposure time [us] (Default: Auto)")
-    parser.add_argument("-g", "--gain", type=float, default=-1, help="Gain [dB] (Default: Auto)")
-    parser.add_argument("-G", "--gamma", type=float, help="Gamma value")
-    parser.add_argument("-b", "--brightness", type=float, help="Brightness [EV]")
-    parser.add_argument("-f", "--fps", type=float, help="FrameRate [fps]")
-    parser.add_argument("-s", "--scale", type=float, default=0.25, help="Image scale to show (>0) (Default: 0.25)")
-    args = parser.parse_args()
-
     # Instance creation
-    cap = EasyPySpin.VideoCapture(args.index)
+    cap = EasyPySpin.VideoCapture(0)
 
     # Checking if it's connected to the camera
     if not cap.isOpened():
@@ -22,18 +14,15 @@ def main():
         return -1
     
     # Set the camera parameters
-    cap.set(cv2.CAP_PROP_EXPOSURE, args.exposure) #-1 sets exposure_time to auto
-    cap.set(cv2.CAP_PROP_GAIN, args.gain) #-1 sets gain to auto
-    if args.gamma      is not None: cap.set(cv2.CAP_PROP_GAMMA, args.gamma)
-    if args.fps        is not None: cap.set(cv2.CAP_PROP_FPS, args.fps)
-    if args.brightness is not None: cap.set(cv2.CAP_PROP_BRIGHTNESS, args.brightness)
+    cap.set(cv2.CAP_PROP_EXPOSURE, -1) #-1 sets exposure_time to auto
+    cap.set(cv2.CAP_PROP_GAIN, -1) #-1 sets gain to auto
 
     # Start capturing
     while True:
         ret, frame = cap.read()
         #frame = cv2.cvtColor(frame, cv2.COLOR_BayerBG2BGR) #for RGB camera demosaicing
 
-        img_show = cv2.resize(frame, None, fx=args.scale, fy=args.scale)
+        img_show = cv2.resize(frame, None, fx=0.25, fy=0.25)
         cv2.imshow("press q to quit", img_show)
         key = cv2.waitKey(30)
         if key==ord("q"):
